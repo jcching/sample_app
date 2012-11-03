922b9bf7645784542ae06f0c74a0139619c20a12
John Ching <jcching135246@gmail.com>
2012-11-03 12:06:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Sample App: by ceapas
+A simple sample application for the drployment of static pages.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3eae5b3123c6e6eaa33f2b51a561f8a34acb31b55158b154c4095c186ad9cea8d1468ac65b113a0e65bd37c6758309e80bef411e084f92956e9465807d3392a5'
